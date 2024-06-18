f01e04faa9196684ce26b892971913335412772f
konradmh3 <konradm.h.3@gmail.com>
2024-06-18 14:19:13 -0700

    made restaurants page async bec we have useSearchParams

--- pickup/src/app/dashboard/restaurants/page.tsx
+++ pickup/src/app/dashboard/restaurants/page.tsx
@@ -2,7 +2,7 @@ import Search from '@/app/ui/search';
 import { lusitana } from '@/app/ui/fonts';
 
 
-export default function restaurants(){
+export default async function restaurants(){
     return <div className="w-full">
     <div className="flex w-full items-center justify-between">
       <h1 className={`${lusitana.className} text-2xl`}>Restaurants</h1>
