6af54400f7d9d0e6fc34c4f4ee393ffa8aba038c
konradmh3 <konradm.h.3@gmail.com>
2024-06-18 14:13:39 -0700

    Added searching for restaurants which adds to url params in order to query from the data base and find the restaurant that you want

--- pickup/src/app/dashboard/restaurants/page.tsx
+++ pickup/src/app/dashboard/restaurants/page.tsx
@@ -1,4 +1,22 @@
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+
+
 export default function restaurants(){
-    return <>Restaurants</>;
+    return <div className="w-full">
+    <div className="flex w-full items-center justify-between">
+      <h1 className={`${lusitana.className} text-2xl`}>Restaurants</h1>
+    </div>
+    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+      <Search placeholder="Search restaurants..." />
+      {/* <CreateInvoice /> */}
+    </div>
+     {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Table query={query} currentPage={currentPage} />
+    </Suspense> */}
+    <div className="mt-5 flex w-full justify-center">
+      {/* <Pagination totalPages={totalPages} /> */}
+    </div>
+  </div>;
     }
     
