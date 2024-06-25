21a396b950785f8788d5c9fc812379dacfbe8f92
konradmh3 <konradm.h.3@gmail.com>
2024-06-25 14:32:02 -0700

    after much testing actually fixed the background:

--- pickup/src/app/dashboard/layout.tsx
+++ pickup/src/app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import Nav from "@/app/ui/nav";
 
 export default function Layout({ children }: { children: React.ReactNode }){
     return (
-    <div className="flex  flex-col md:h-screen md:flex-row md:overflow-hidden">
+    <div className="flex flex-col overflow-y-auto h-screen md:flex-row md:overflow-hidden">
         <div className="flex-none  md:w-64">
             <Nav/>
         </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        <div className="flex-grow p-6  md:p-12">
             {children}
         </div>
     </div>
--- pickup/src/app/dashboard/restaurants/page.tsx
+++ pickup/src/app/dashboard/restaurants/page.tsx
@@ -1,14 +1,11 @@
-import Search from '@/app/ui/search';
-import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchRestaurantPages } from '@/app/lib/data';
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchRestaurantPages } from "@/app/lib/data";
 
-import Table from '@/app/ui/table';
-import Pagination from '@/app/ui/pagination';
-
-
-
+import Table from "@/app/ui/table";
+import Pagination from "@/app/ui/pagination";
 
 export default async function restaurants({
   searchParams,
@@ -18,31 +15,30 @@ export default async function restaurants({
     page?: string;
   };
 }) {
-
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page || 1);
   // above his how to recieve something from the url
 
   const totalPages = await fetchRestaurantPages(query);
 
-
-    return <div className="w-full">
-    <div className="flex w-full items-center justify-between">
-      <h1 className={`${lusitana.className} text-2xl`}>Restaurants</h1>
-    </div>
-    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-      <Suspense fallback={null}>
-        {/* create skeleton for the above */}
-        <Search placeholder="Search restaurants..." />
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Restaurants</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Suspense fallback={null}>
+          {/* create skeleton for the above */}
+          <Search placeholder="Search restaurants..." />
+        </Suspense>
+        {/* <CreateInvoice /> */}
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
       </Suspense>
-      {/* <CreateInvoice /> */}
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
     </div>
-     <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-      <Table query={query} currentPage={currentPage} />
-    </Suspense>
-    <div className="mt-5 flex w-full justify-center">
-      <Pagination totalPages={totalPages} />
-    </div>
-  </div>;
-    }
-    
+  );
+}
