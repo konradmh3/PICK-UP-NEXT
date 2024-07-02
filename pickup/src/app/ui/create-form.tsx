db3b1e502746bade4e1f22a5dbcc6955fa61b9c6
konradmh3 <konradm.h.3@gmail.com>
2024-07-02 13:23:05 -0700

    Added automatic formating for phone number so unformatted or incorrect inpts literally cant be entered

--- pickup/src/app/ui/create-form.tsx
+++ pickup/src/app/ui/create-form.tsx
@@ -1,17 +1,28 @@
+'use client';
+
 // import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+import Link from "next/link";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
-import { createRestaurant } from '@/app/lib/actions';
+} from "@heroicons/react/24/outline";
+import { Button } from "@/app/ui/button";
+import { createRestaurant } from "@/app/lib/actions";
 
-export default function Form(
-  // { customers }: { customers: CustomerField[] }
-) {
+export default function Form() {
+
+  function handlePhoneChange(e: React.ChangeEvent<HTMLInputElement>){
+    e.target.value = e.target.value.replace(/\D/g, '');
+    if(e.target.value.length > 3 && e.target.value.length <= 6){
+      e.target.value = `(${e.target.value.slice(0,3)})${e.target.value.slice(3,6)}`;
+    }else if (e.target.value.length > 6){
+      e.target.value = `(${e.target.value.slice(0,3)})${e.target.value.slice(3,6)}-${e.target.value.slice(6,10)}`;
+    }
+  }
+
+
   return (
     <form action={createRestaurant}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -62,22 +73,19 @@ export default function Form(
               <input
                 id="phone"
                 name="phone"
-                type="string"
+                type="tel"
                 pattern="\([0-9]{3}\)[0-9]{3}-[0-9]{4}"
                 // required
                 placeholder="(xxx)xxx-xxxx"
+                minLength={9}
+                maxLength={13}
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-2 text-sm outline-2 placeholder:text-gray-500"
+                onChange={handlePhoneChange}
               />
               {/* <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
             </div>
           </div>
         </div>
-
-        
-
-
-
-
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
