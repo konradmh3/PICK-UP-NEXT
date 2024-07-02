d36dc038c71f14b43338c2264a63ba0f53d71a85
konradmh3 <konradm.h.3@gmail.com>
2024-07-02 14:05:23 -0700

    Changed min dollar amount to 0

--- pickup/src/app/ui/create-form-menu.tsx
+++ pickup/src/app/ui/create-form-menu.tsx
@@ -87,7 +87,8 @@ export default function Form({
                 id="amount"
                 name="amount"
                 type="number"
-                step="0.01"
+                step="0.25"
+                min={0}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
